8b4a5f584fcac8f18f9efdb5f3e8d8ebead6a4fd
loronto <46075089+loronto@users.noreply.github.com>
2023-02-05 23:18:25 +0530

    added some bootstrap

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,40 +1,69 @@
 import './App.css';
-import { useState,useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import MoviePoster from './Component/MoviePoster';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 
 function App() {
-  const [movies,setMovies] = useState([]);
-  const [search,setSearch] = useState('');
-  const fetchMovies = async () =>
-  {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
+  const [favourites, setFavourites] = useState([]);
+  const searchInput = useRef();
+
+  const fetchMovies = async () => {
     const getReq = `http://www.omdbapi.com/?s=${search}&apikey=7f7ea64d`;
-    console.log(getReq);
     const response = await fetch(getReq);
     const responseJson = await response.json();
-    if(responseJson.Search)
-    setMovies(responseJson.Search);
+    if (responseJson.Search)
+      setMovies(responseJson.Search);
+  }
+
+  const processFavourite = (movie) => {
+    if (favourites.includes(movie) == false) {
+      setFavourites([...favourites, movie]);
+    }
+    else {
+      const newFav = [...favourites];
+      newFav.splice(favourites.indexOf(movie), 1);
+      setFavourites(newFav);
+    }
   }
 
   useEffect(
-    () =>
-    {
+    () => {
       fetchMovies();
-    },[search]
+    }, [search]
   );
 
+  function doSearch() {
+    setSearch(searchInput.current.value);
+  }
   return (
     <div className="App">
-      <div className='Header'>
-        <div className='logo-container'>
-        <img src='filmLogo.png' className='logo'/> <div className='movieTitle'>Movie Title</div>
+
+      <nav className='navbar navbar-dark bg-dark'>
+        <span className='navbar-brand'>
+          <img src="filmLogo.png" height="30" width="30" className="d-inline-block align-top Logo" />
+          Movie Title
+        </span>
+        <div className="form-inline SearchSection">
+          <div className='input-group'>
+            <input ref={searchInput} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" onClick={doSearch}>Search</button>
+          </div>
         </div>
-        <input className='searchBox' type="text" placeholder='Search...' onChange={(event) => setSearch(event.target.value)}/>
+      </nav>
+
+      <div className='.d-flex flex-column align-items-center'>
+        <div className='p-1'>
+          <MoviePoster movies={movies} favouriteHandler={processFavourite}></MoviePoster>
+        </div>
       </div>
-      <div className='Poster-container'>
-      <MoviePoster movies = {movies}></MoviePoster>
+      <div className='Poster-container Favourites-container'>
+        <MoviePoster movies={favourites} favouriteHandler={processFavourite}></MoviePoster>
       </div>
+
     </div>
   );
 }
--- movie-app/src/Component/MoviePoster.js
+++ movie-app/src/Component/MoviePoster.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-const MoviePoster = (props) =>
-{
+const MoviePoster = (props) => {
     return (<>
-    {props.movies.map(
-        (movie) => (
+        {props.movies.map(
+            (movie) => (
                 <div className="Poster">
-                <img src = {movie.Poster} className = "PosterImage"></img>
+                    <img src={movie.Poster} className="PosterImage" onError={(target)=>{
+                        target.onError = null;
+                        target.src = "movie-fill.svg";
+                    }}></img>
+                    <div className='Favourites'>
+                        <div className="WrapLike">
+                            <img src="heart-fill-blue.svg" className="LikeImg" onClick={() => props.favouriteHandler(movie)}/><div className="Like"> Add to Favourites</div>
+                        </div>
+                    </div>
                 </div>
-        )
-    )}</>);
+            )
+        )}</>);
 }
 
 export default MoviePoster;
