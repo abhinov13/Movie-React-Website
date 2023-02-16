22b9f626d450aa1a30a56d2db28462d232bd9126
abhinov13 Capgemini <abhinovpm@gmail.com>
2023-02-16 22:01:10 +0530

    added mouseleave and incomplete persistence

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -7,78 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
-  const [favourites, setFavourites] = useState([
-    {
-      "Title": "Captain America: The First Avenger",
-      "Year": "2011",
-      "imdbID": "tt0458339",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "The Toxic Avenger",
-      "Year": "1984",
-      "imdbID": "tt0090190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzViNmQ5MTYtMmI4Yy00N2Y2LTg4NWUtYWU3MThkMTVjNjk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "The Toxic Avenger Part II",
-      "Year": "1989",
-      "imdbID": "tt0098503",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BODhiNTljYTctMmIzZC00ZGVkLTk2NmItN2RjMzY3ZTYyNDczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Citizen Toxie: The Toxic Avenger IV",
-      "Year": "2000",
-      "imdbID": "tt0212879",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMWI0NWY0ODUtNGY3Yy00ZDU1LTllYjUtMDFkYWEzZGQwY2I1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "The Toxic Avenger Part III: The Last Temptation of Toxie",
-      "Year": "1989",
-      "imdbID": "tt0098502",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNjVlNzFjMGItMTEwYy00OTc0LWFmY2ItM2U0MmQyYWI5Njk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Avenger",
-      "Year": "2006",
-      "imdbID": "tt0473445",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTMzMjMwMjcyNl5BMl5BanBnXkFtZTcwNTA1NDgzMQ@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Knives of the Avenger",
-      "Year": "1966",
-      "imdbID": "tt0059045",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzcxYjhjMDAtZmY4Yi00NmJlLTg4NDItNDBmYzljYzA3MmJmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Samurai Avenger: The Blind Wolf",
-      "Year": "2009",
-      "imdbID": "tt1243972",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTAwNjc4MTkyNjBeQTJeQWpwZ15BbWU3MDg3NTQyMzI@._V1_SX300.jpg"
-    },
-    {
-      "Title": "The Avenger",
-      "Year": "1962",
-      "imdbID": "tt0056174",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjNmOTEzN2YtYTcyMC00NDQ1LTg5NTMtMjQ3M2ZlOGU2YmFkXkEyXkFqcGdeQXVyMzg1ODEwNQ@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "The Avenger",
-      "Year": "1960",
-      "imdbID": "tt0054257",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzU5YzM3MmEtNTE2MS00MzVjLWI5Y2EtNGU3M2YwMGYzMGQ0XkEyXkFqcGdeQXVyMDExMzA0Mw@@._V1_SX300.jpg"
-    }
-  ]);
+  const [favourites, setFavourites] = useState([]);
 
   const [movies, setMovies] = useState([
     {
@@ -155,9 +84,18 @@ function App() {
   const [search, setSearch] = useState('');
   const [poster,setPoster] = useState('');
 
+  useEffect(
+    () =>
+    {
+      const fav = JSON.parse(localStorage.getItem("movieFavourites"));
+      setFavourites(fav);
+    },[]
+  )
+
   const searchInput = useRef();
 
   const fetchMovies = async () => {
+    console.log("trying to fetch");
     const getReq = `http://www.omdbapi.com/?s=${search}&apikey=7f7ea64d`;
     console.log(getReq);
     const response = await fetch(getReq);
@@ -177,6 +115,7 @@ function App() {
       newFav.splice(favourites.indexOf(movie), 1);
       setFavourites(newFav);
     }
+    localStorage.setItem("movieFavourites",JSON.stringify(favourites));
   }
 
   const updateBackground = (poster) =>
@@ -184,13 +123,10 @@ function App() {
     setPoster(poster);
   }
 
-  useEffect(
-    () =>
-    {
-      console.log("fre effect " + poster);
-
-    },[poster]
-  )
+  const resetBackground = () =>
+  {
+    setPoster("https://images.pling.com/img/00/00/59/97/06/1588502/0448be61263aa99d9f2e221bb434d120a2c7ebb00917c9e583886c32bb1025dd3da2.jpg");
+  }
 
   useEffect(
     () => {
@@ -207,8 +143,8 @@ function App() {
       <div className='backgroundImg' style={{backgroundImage: 'url('+poster+')'}}></div>
       <nav className='navbar navbar-dark customNav'>
         <span className='navbar-brand'>
-          <img src="filmLogo.png" height="30" width="30" className="d-inline-block align-top Logo" />
-          Movie Title
+          <img src="logo.svg" height="30" width="30" className="d-inline-block align-top Logo" />
+          _Movie Rating__
         </span>
         <div className="form-inline SearchSection">
           <div className='input-group'>
@@ -218,10 +154,10 @@ function App() {
         </div>
       </nav>
       <div className='Movie-container'>
-        <MoviePoster movies={movies} favourites={favourites} favouriteHandler={processFavourite} updateBackground={updateBackground}></MoviePoster>
+        <MoviePoster movies={movies} favourites={favourites} favouriteHandler={processFavourite} updateBackground={updateBackground} resetBackground={resetBackground}></MoviePoster>
       </div>
       <div className='Favourites-container'>
-        <MoviePoster movies={favourites} favourites={favourites} favouriteHandler={processFavourite} updateBackground={updateBackground}></MoviePoster>
+        <MoviePoster movies={favourites} favourites={favourites} favouriteHandler={processFavourite} updateBackground={updateBackground} resetBackground={resetBackground}></MoviePoster>
       </div>
     </div>
   );
--- movie-app/src/Component/MoviePoster.js
+++ movie-app/src/Component/MoviePoster.js
@@ -5,25 +5,31 @@ const MoviePoster = (props) => {
         <div className="d-flex flex-row h-100 Poster-container">
             {props.movies.map(
                 (movie) => (
-                    <div className="mt-3 mx-3 Poster-border" onMouseEnter={() => props.updateBackground(movie.Poster)} onMouseLeave={()=>{props.resetBackground()}}>
+                    <div className="mt-3 mx-3 Poster-border" onMouseEnter={() => props.updateBackground(movie.Poster)} onMouseLeave={() => { props.resetBackground() }}>
+
                         <div className="Poster">
-                            <img src={movie.Poster} className="PosterImage h-100"/>
+                            <a href={"https://www.imdb.com/title/" + movie.imdbID + "/"} target="_blank" >
+                                <img src={movie.Poster} className="PosterImage h-100" />
+                            </a>
                             <div className='Favourites w-100'>
                                 <div className="WrapLike" onClick={() => props.favouriteHandler(movie)}>
-                                {
-                                    props.favourites.includes(movie)?
-                                    <>
-                                        <img src="heart-fill-red.svg" className="LikeImg"/>
-                                        <span className="Like"> Add to Favourites</span>
-                                    </>
-                                    :
-                                    <>
-                                        <img src="heart-fill-blue.svg" className="LikeImg" />
-                                        <span className="Like"> Add to Favourites</span>
-                                    </>
-                                }
+                                    {
+                                        props.favourites.includes(movie) ?
+                                            <>
+                                                <img src="heart-fill-red.svg" className="LikeImg" />
+                                                <span className="Like"> Add to Favourites</span>
+                                            </>
+                                            :
+                                            <>
+                                                <img src="heart-fill-blue.svg" className="LikeImg" />
+                                                <span className="Like"> Add to Favourites</span>
+                                            </>
+                                    }
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="MovieInfo">
                         </div>
                     </div>
                 )
