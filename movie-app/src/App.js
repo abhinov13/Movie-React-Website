b566be1d6464f977863a5df8c0c4684973ef8685
loronto <46075089+loronto@users.noreply.github.com>
2023-02-21 02:14:49 +0530

    made navbar responsive and a litte more adjustment

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [favourites, setFavourites] = useState([]);
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState('');
-  const [poster,setPoster] = useState('');
+  const [poster, setPoster] = useState('');
 
   const searchInput = useRef();
 
@@ -22,18 +22,17 @@ function App() {
       setMovies(responseJson.Search);
   }
 
-  const addToLocal = () =>
-  {
-    localStorage.setItem("favmovielist",JSON.stringify(favourites));
+  const addToLocal = () => {
+    localStorage.setItem("favmovielist", JSON.stringify(favourites));
   }
 
   useEffect(
-    () =>
-    {
-      const p =JSON.parse(localStorage.getItem("favmovielist"));
+    () => {
+      const p = JSON.parse(localStorage.getItem("favmovielist"));
       setFavourites(p);
+      resetBackground();
       setSearch("fast");
-    },[]
+    }, []
   )
 
 
@@ -51,28 +50,23 @@ function App() {
   }
   const executed = useRef(false);
   useEffect(
-    () =>
-    {
-      if(executed.current == false)
-      {
+    () => {
+      if (executed.current == false) {
         executed.current = true;
       }
-      else
-      {
+      else {
         addToLocal();
       }
-      
-    },[favourites]
+
+    }, [favourites]
   )
 
-  const updateBackground = (poster) =>
-  {
-    if(poster != "N/A")
-    setPoster(poster);
+  const updateBackground = (poster) => {
+    if (poster != "N/A")
+      setPoster(poster);
   }
 
-  const resetBackground = () =>
-  {
+  const resetBackground = () => {
     setPoster("https://images.pling.com/img/00/00/59/97/06/1588502/0448be61263aa99d9f2e221bb434d120a2c7ebb00917c9e583886c32bb1025dd3da2.jpg");
   }
 
@@ -83,25 +77,36 @@ function App() {
   );
 
 
-  function doSearch() {
+  const doSearch = (e) => {
+    e.preventDefault();
+    console.log("trying to search " + searchInput.current.value);
     setSearch(searchInput.current.value);
   }
 
   return (
     <div className="App user-select-none">
-      <div className='backgroundImg' style={{backgroundImage: 'url('+poster+')'}}></div>
-      <nav className='navbar navbar-dark customNav'>
+      <div className='backgroundImg' style={{ backgroundImage: 'url(' + poster + ')' }}></div>
+
+      <nav className="navbar navbar-expand-lg navbar-dark customNav">
         <span className='navbar-brand'>
           <img src="logo.svg" height="30" width="30" className="d-inline-block align-top Logo" />
           _Movie Rating__
         </span>
-        <div className="form-inline SearchSection">
-          <div className='input-group'>
-            <input ref={searchInput} className="form-control" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-success my-2 my-sm-0" onClick={doSearch}>Search</button>
-          </div>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-bs-target="#navbarToggler" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse searchContainer" id="navbarToggler">
+          <form className="searchBar">
+            <div className='input-group'>
+              <input ref={searchInput} className="form-control" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-success" onClick={(e)=>{doSearch(e)}}>Search</button>
+            </div>
+          </form>
         </div>
       </nav>
+
+
+
       <div className='Movie-container'>
         <MoviePoster movies={movies} favourites={favourites} favouriteHandler={processFavourite} updateBackground={updateBackground} resetBackground={resetBackground}></MoviePoster>
       </div>
